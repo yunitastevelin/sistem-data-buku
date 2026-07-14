f7651e45d9f900435a19a9fc5b7f3e6195c44e8d
Yunita Stevelin <yunitastevelin@gmail.com>
2026-07-14 15:10:57 +0700

    Initial Laravel setup and dashboard

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Sistem Data Buku</title>
+</head>
+
+<body>
+    <h1>📚 Sistem Data Buku</h1>
+    <hr>
+
+    <h3>Menu</h3>
+
+    <ul>
+        <li>Dashboard</li>
+        <li>Data Buku</li>
+        <li>Data Anggota</li>
+        <li>Peminjaman</li>
+        <li>Pengembalian</li>
+    </ul>
+
+</body>
+
+</html>
