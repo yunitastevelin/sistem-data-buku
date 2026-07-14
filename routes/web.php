c17a76b16aac74d6b4b50f4736887d2d5a080d99
Yunita Stevelin <yunitastevelin@gmail.com>
2026-07-14 18:48:35 +0700

    Menambahkan Route Resource Kategori Dan Buku

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\BukuController;
 
-Route::get('/', [DashboardController::class, 'index']);
+Route::get('/', function () {
+    return redirect('/kategori');
+});
+
+Route::resource('kategori', KategoriController::class);
+Route::resource('buku', BukuController::class);
