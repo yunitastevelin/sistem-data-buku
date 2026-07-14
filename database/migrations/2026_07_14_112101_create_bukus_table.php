262c7562a8b6411c6bc18c5098cc09e6babf9e48
Yunita Stevelin <yunitastevelin@gmail.com>
2026-07-14 18:34:38 +0700

    Menambahkan Migration Tabel Kategori Dan Buku

---
+++ database/migrations/2026_07_14_112101_create_bukus_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bukus', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('kategori_id')->constrained('kategoris')->onDelete('cascade');
+
+            $table->string('judul');
+            $table->string('penulis');
+            $table->string('penerbit');
+            $table->year('tahun_terbit');
+            $table->integer('stok');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bukus');
+    }
+};
